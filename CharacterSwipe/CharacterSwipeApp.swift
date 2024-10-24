67b80e8d61e5439578091a5319b40ad1c6d7a890
Liam Nagel <>
2024-10-23 18:23:39 -0700

    Setup

---
+++ CharacterSwipe/CharacterSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterSwipeApp.swift
+//  CharacterSwipe
+//
+//  Created by Liam Nagel on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CharacterSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CharacterSwipeTests/CharacterSwipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterSwipeTests.swift
+//  CharacterSwipeTests
+//
+//  Created by Liam Nagel on 10/23/24.
+//
+
+import Testing
+@testable import CharacterSwipe
+
+struct CharacterSwipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
