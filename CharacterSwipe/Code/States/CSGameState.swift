5dc82111eb5226db0d8d495c01abb170b9353513
Adam <adamneula@gmail.com>
2024-10-30 09:28:41 -0700

    finished gameboard initialization and laid out framework for the moves

--- CharacterSwipe/Code/States/CSGameState.swift
+++ CharacterSwipe/Code/States/CSGameState.swift
@@ -17,7 +17,30 @@ class gameBoard {
                                [0, 0, 0, 0],
                                [0, 0, 0, 0]]) {
         self.gameBoard = gameBoard
-        self.gameBoard[Int.random(in 0...2)][Int.random(in 0...2)] = [2, 4].randomElement()!
+        self.gameBoard[Int.random(in: 0..<4)][Int.random(in: 0..<4)] = [2, 4].randomElement()!
+        while(true) {
+            let randomRow = Int.random(in: 0..<4)
+            let randomColumn = Int.random(in: 0..<4)
+
+            if gameBoard[randomRow][randomColumn] == 0 {
+                self.gameBoard[randomRow][randomColumn] = [2, 4].randomElement()!
+                break
+            }
+        }
     }
     
+    func swipe(direction: String) {
+        if direction == "right" {
+            
+        }
+        else if direction == "left" {
+            
+        }
+        else if direction == "up" {
+            
+        }
+        else if direction == "down" {
+            
+        }
+    }
 }
